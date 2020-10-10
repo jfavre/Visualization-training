fd32e58119017d05a4dc10e228c4bf1ee042118a
Jean M. Favre <jfavre@cscs.ch>
2020-10-10 18:56:15 +0200

    more time dependent stuff

--- ParaView/pvGenerateImageData.py
+++ ParaView/pvGenerateImageData.py
@@ -15,29 +15,16 @@ import numpy as np
 executive = self.GetExecutive()
 outInfo = executive.GetOutputInformation(0)
 exts = [executive.UPDATE_EXTENT().Get(outInfo, i) for i in range(6)]
-whole = [executive.WHOLE_EXTENT().Get(outInfo, i) for i in range(6)]
 ts = executive.UPDATE_TIME_STEP().Get(outInfo)
 dims = (exts[1]-exts[0]+1, exts[3]-exts[2]+1, exts[5]-exts[4]+1)
-global_dims = [whole[1]-whole[0]+1, whole[3]-whole[2]+1, whole[5]-whole[4]+1]
+
 output.SetExtent(exts)
-#pid = outInfo.Get(executive.UPDATE_PIECE_NUMBER())
-#gl = outInfo.Get(executive.UPDATE_NUMBER_OF_GHOST_LEVELS())
-#f = open(format("/tmp/n.%d.txt" % pid), "w")
-#f.write(format("pid = %d: " % pid))
-#f.write(format("exts = (%d,%d,%d,%d,%d,%d)" % (exts[0], exts[1],exts[2], exts[3],exts[4], exts[5])))
-#f.write(format("whole = (%d,%d,%d,%d,%d,%d)" % (whole[0], whole[1],whole[2], whole[3],whole[4], whole[5])))
-#f.write(format("dims = %d,%d,%d" % (dims[0], dims[1],dims[2])))
-xaxis = np.linspace(-.5, 1., global_dims[0])[exts[0]:exts[1]+1]
-yaxis = np.linspace(-1.,1., global_dims[1])[exts[2]:exts[3]+1]
-zaxis = np.linspace(-1., .5, global_dims[2])[exts[4]:exts[5]+1]
+xaxis = np.linspace(-.5, 1., dims[0])
+yaxis = np.linspace(-1.,1., dims[1])
+zaxis = np.linspace(-1., .5, dims[2])
 [xc,yc,zc] = np.meshgrid(zaxis,yaxis,xaxis, indexing="ij")
 data = .1*ts +sin(ts) + np.sqrt(xc**2 + yc**2 + zc**2, dtype='f')
-#data2 = ts + np.sqrt(xc**2 + yc**4 + 3*zc**2, dtype='f')
-#ids = np.reshape(range(np.prod(global_dims)), global_dims)[exts[4]:exts[5]+1,exts[2]:exts[3]+1,exts[0]:exts[1]+1]
-output.PointData.append(data.ravel(), "A")
-#output.PointData.append(data2.ravel(), "B")
-#output.PointData.append(ids.ravel(), "GlobalNodeIds")
-#f.close()
+output.PointData.append(data.ravel(), "scalarA")
 """
 
 RequestInfo = """
